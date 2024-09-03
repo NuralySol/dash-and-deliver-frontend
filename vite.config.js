7cfbdf43f48199f7c41cfe5b4d7ab8efa4feb793
NuralySol <soltonbekov@gmail.com>
2024-09-03 10:30:43 -0400

    changed logo to aliasing to access the correct path

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import path from 'path';
 
 export default defineConfig({
   plugins: [react()],
@@ -9,6 +10,11 @@ export default defineConfig({
   build: {
     rollupOptions: {
       // Do not include these as external, allow Vite to bundle them
+    },
+  },
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
     },
   },
 });
